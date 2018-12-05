1431cb10fb9b5f9ee34454cc63bf5c9016d8e54b
Gees Brouwer <brouwer.gees@gmail.com>
2018-12-05 15:17:48 +0100

    Day 1 - part 2

--- day1.js
+++ day1.js
@@ -1,1 +1,19 @@
-console.log(require('fs').readFileSync('input.txt').toString().split("\n").reduce((a,b) => +a + +b));
+const input = require('fs').readFileSync('input.txt').toString().split("\n").map(n => +n);
+
+// Part 1
+console.log(input.reduce((a,b) => a + b));
+
+
+// Part 2
+const occ = {};
+let sum = 0;
+while(true) {
+    for(let i of input) {
+        sum += i;
+        if(occ[sum]) return console.log(sum);
+        occ[sum] = true;
+    }
+}
+
+// const occ = {};
+// input.reduce((a,b) => { occ[a] = true; if(occ[a]) { console.log(a) || process.exit(); } return a + b; });
